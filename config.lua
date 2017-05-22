9b2a0afa5850404d6381b29862d6d16ac28fafd6
Chalwk <jericho.crosby227@gmail.com>
2017-05-22 19:34:47 +1200

    updated build.settings | config.lua
    Signed-off-by: Chalwk <jericho.crosby227@gmail.com>

---
+++ config.lua
@@ -0,0 +1,15 @@
+-- config.lua for Asteroids-2017
+
+application = {
+    content =
+	{
+        width = 320,
+        height = 480,
+		fps = 60,
+		antialias = true,
+		imageSuffix =
+		{
+			["@2x"] = 2,
+		},
+	},
+}
